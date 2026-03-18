cdbd8fe48ec6faed491d4d7c126a16390070fd19
OjusThakur <ojusthakur1106@gmail.com>
2026-03-18 23:58:19 +0530

    [Ojus] commit add

---
+++ String/level_1/NullPointer.java
@@ -0,0 +1,26 @@
+package com.gla.String;
+
+public class NullPointer {
+    public static void generateException() {
+        String text = null;
+        System.out.println(text.length());
+    }
+
+    public static void handleException() {
+
+        String text = null;
+
+        try {
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("Exception Handled: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+
+        handleException();
+    }
+}
---
+++ String/level_2/Word.java
@@ -0,0 +1,34 @@
+package com.gla.String.level_2;
+
+import java.util.Scanner;
+public class Word {
+    public static void findWords(String[] words) {
+
+        String shortest = words[0];
+        String longest = words[0];
+
+        for (int i = 1; i < words.length; i++) {
+
+            if (words[i].length() < shortest.length())
+                shortest = words[i];
+
+            if (words[i].length() > longest.length())
+                longest = words[i];
+        }
+
+        System.out.println("Shortest word: " + shortest);
+        System.out.println("Longest word: " + longest);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter sentence: ");
+        String text = sc.nextLine();
+
+        String[] words = text.split(" ");
+
+        findWords(words);
+    }
+}
